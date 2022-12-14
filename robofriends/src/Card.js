4fdbc681f2ff159d9d6de18ad025ffae4145ac14
unknown <tudorbarnsley@gmail.com>
2022-12-14 09:19:42 +0000

    using string template and destructuring using const

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -2,6 +2,7 @@
 import React from "react";
 
 const Card = (props) => {
+    const {name, email, id} = props; 
     return (
         //we need to make sure that we 
         //return one thing
@@ -9,10 +10,10 @@ const Card = (props) => {
         //putting an h1 here won't work
 
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src='https://robohash.org/test?200x200'/>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
