b404e249918845c160bc96d4cf45abce408086f3
unknown <tudorbarnsley@gmail.com>
2022-12-14 10:42:49 +0000

    made Searchbox and added a bit of css

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import CardList from "./CardList";
+import CardList from './CardList';
+import Searchbox from './Searchbox';
 import {robots} from './robots';
 
 const App = () => {
     return (
+        <div>
+        <h1 className="tc">RoboFriends</h1>
+        <Searchbox />
         <CardList robots={robots}/>
+        </div>
     )
 }
 
