42abf646cd936b78ffaa25a55b6aa55517e3e6ea
unknown <tudorbarnsley@gmail.com>
2022-12-14 10:31:22 +0000

    made App father of child CardList

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import Card from './Card';
-import CardList from './CardList';
+//import CardList from './CardList';
+import App from './App';
+
 import 'tachyons';
 
 //getting info on robots 
@@ -26,7 +28,7 @@ root.render(
 
   <React.StrictMode>
     <div>
-      <CardList robots={robots}/>    
+      <App />    
       {/* <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
       <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
       <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
