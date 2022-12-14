3b2453e8fa44090f2a05f34353a0cc2b6516f5e3
unknown <tudorbarnsley@gmail.com>
2022-12-14 10:20:28 +0000

    make code cleaner less variables moved into {}

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -7,30 +7,54 @@ const CardList = ({ robots }) => {
 
     //lets get all robots my using map to loop over them
 
-    const cardArray= robots.map((user, i) => {
-        // we use variable i for index so we get 0,1,2....9
-        // remember! user just replaces the idea 
-        // of what is there in the object;
-        // use "user" as a default for mapping anything!
+    // const cardArray= robots.map((user, i) => {
+    //     // we use variable i for index so we get 0,1,2....9
+    //     // remember! user just replaces the idea 
+    //     // of what is there in the object;
+    //     // use "user" as a default for mapping anything!
 
-        return (
-            <Card
-                key={i}
-                id={robots[i].id}
-                name={robots[i].name}
-                email={robots[i].email}
-            />
-        );
+    //     return (
+    //         <Card
+    //             key={i}
+    //             id={robots[i].id}
+    //             name={robots[i].name}
+    //             email={robots[i].email}
+    //         />
+    //     );
 
-    })
+    // })
 
     return (
         <div>
+
+
             {/* remember: JavaScript needs to know
     to compile cardComponent so 
     it needs to be put in brakets */}
 
-            {cardArray}
+            {/* {cardArray} */}
+
+            {
+                /* WE REMOVE one variable by taking everyhting
+                and putting it here inside the evaluation brakets {}
+                it works same as using const ..... */
+                robots.map((user, i) => {
+                    // we use variable i for index so we get 0,1,2....9
+                    // remember! user just replaces the idea 
+                    // of what is there in the object;
+                    // use "user" as a default for mapping anything!
+
+                    return (
+                        <Card
+                            key={i}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    );
+
+                })
+            }
 
         </div>
     )
