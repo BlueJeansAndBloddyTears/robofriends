b1adcf5d54a7e7f330cbe30f412c7a1bdd9bab7f
unknown <tudorbarnsley@gmail.com>
2022-12-14 10:14:21 +0000

    renamed cardComponent to cardArray, fixed warning unique key prop

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -7,13 +7,20 @@ const CardList = ({ robots }) => {
 
     //lets get all robots my using map to loop over them
 
-    const cardComponent = robots.map((user, i) => {
+    const cardArray= robots.map((user, i) => {
         // we use variable i for index so we get 0,1,2....9
         // remember! user just replaces the idea 
         // of what is there in the object;
         // use "user" as a default for mapping anything!
 
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+        return (
+            <Card
+                key={i}
+                id={robots[i].id}
+                name={robots[i].name}
+                email={robots[i].email}
+            />
+        );
 
     })
 
@@ -23,10 +30,14 @@ const CardList = ({ robots }) => {
     to compile cardComponent so 
     it needs to be put in brakets */}
 
-            {cardComponent}
+            {cardArray}
 
         </div>
     )
 }
 
+//Changes:
+//cardComponent name to cardArray 
+// Resolved Warning: Each child in 
+// a list should have a unique "key" prop.
 export default CardList;
