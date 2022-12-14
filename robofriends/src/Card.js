c8e79a74ebeeae57d56a859d97fe7c9473a6b213
unknown <tudorbarnsley@gmail.com>
2022-12-14 09:34:05 +0000

    added info on React Fragments and when to use them

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,6 +1,47 @@
 //import React, {Component} from "react";
 import React from "react";
 
+//import React, {Fragment} from "react";
+
+// when we have multiple elements we
+//can use Fragment in order to avoid
+// adding unnecesarry divs to the DOM
+
+{/* <Fragment>
+<p>Q. What is React?</p>
+<p>A. A JavaScript library for building user interfaces</p>
+<p>Q. How do I render sibling elements?</p>
+<p>A. Use Fragments</p>
+</Fragment> */}
+
+//or using the shorthand <> </> if
+//not using keys
+
+{/* <>
+<p>Q. What is React?</p>
+<p>A. A JavaScript library for building user interfaces</p>
+<p>Q. How do I render sibling elements?</p>
+<p>A. Use Fragments</p>
+</> */}
+
+// The only thing that the shorthand syntax doesn’t support
+// for now is adding a key. This could be problematic
+//if you are creating a description list, for example. 
+//In this case, use the standard syntax.
+
+// function Dictionary(props) {
+//     return (
+//         <dl>
+//             {props.terms.map(({word, definition}) => 
+//                 <React.Fragment key={word}>
+//                     <dt>{word}</dt>
+//                     <dd>{definition}</dd>
+//                 </React.Fragment>
+//             )}
+//         </dl>
+//     );
+// }
+
 const Card = ({name, email, id}) => {
   //  const {name, email, id} = props; 
     return (
