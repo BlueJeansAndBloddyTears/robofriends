239dbd39a3471e34f33d0fa31e92af0af443555a
unknown <tudorbarnsley@gmail.com>
2022-12-14 10:49:40 +0000

    prelude to STATE before changing everything to class components

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -3,6 +3,17 @@ import CardList from './CardList';
 import Searchbox from './Searchbox';
 import {robots} from './robots';
 
+//we need a STATE which describes our application
+// in order to to change what robots get displayed
+//
+
+//PROPS are simply things that come from state
+//the parent tells the child what the state is
+// BUT in order to use STATE we need to go back
+//to the way we created components
+// not like we have now as a function const app ...
+//but as a class which extends react component 
+
 const App = () => {
     return (
         <div>
